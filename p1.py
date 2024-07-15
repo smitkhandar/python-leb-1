55b00cd0bda936b0ab537173d1a1fe0e6a1a91cf
smitkhandar <bca2022smit1695@tnraocollege.org>
2024-07-15 17:09:19 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,10 @@
+num1 = int (input (" Enter the first number:"))
+num2 = int (input("Enter the second number:"))
+sum = num1 + num2
+print(" sum of number : ", sum)
+sub = num1 - num2
+print ("Subtraction of number:", sub)
+mul = num1 * num2
+print (" Multiplication of number:", mul)
+div = num1 / num2
+print (" Division of number:", div)
